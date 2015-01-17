766a5f9dec4df707f893523efb3b4bd0d5b57103
Bardia Jedi <bardiajedi@gmail.com>
2015-01-17 23:24:14 +0100

    abit work on the menu

--- src/pjr01Main/Lunch.java
+++ src/pjr01Main/Lunch.java
@@ -1,5 +1,47 @@
 package pjr01Main;
 
-public class Lunch {
+import java.util.Scanner;
 
+public class Lunch {
+	public static void main (String []args){
+		startup();
+	}
+	
+	private static void startup(){
+		//load 
+		
+		//lunch main menu
+		mainMenu();
+	}
+
+	private static void mainMenu() {
+		//draw
+		System.out.println("Weclcom plase selct an option use help for a help");
+		String test = new String("non");
+		reader();
+	}
+	
+	private static void reader(){
+		Scanner keyboard = new Scanner(System.in);
+		String in = new String(keyboard.nextLine());
+		
+		switch (in.toLowerCase()) {
+		case "help":
+			System.out.println("help");
+			break;
+		
+		case "exit":
+			System.out.println("shuting down");
+			break;
+			
+		case "":
+			reader();
+			break;
+
+		default:
+			System.out.println("WTF is "+in+ "\ntry agen maybe");
+			reader();
+			break;
+		}
+	}
 }
